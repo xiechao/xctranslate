9b2bb0e3de14e50ec96702ba2dc1afd93c784e1d
test <test@test.com>
2018-12-13 14:33:56 +0800

    update

---
+++ data/hdPathType.js
@@ -0,0 +1,16 @@
+module.exports = {
+    hdPath: [
+        {
+            name: "Jaxx, Metamask (ETH)",
+            value: "m/44'/60'/0'/0/0"
+        },
+        {
+            name: "Ledger (ETH)",
+            value: "m/44'/60'/0'/0"
+        },
+        {
+            name: "自定义路径",
+            value: "m/44'/60'/1'/0/0"
+        },
+    ]
+};
--- src/Translate.js
+++ src/Translate.js
@@ -1,12 +1,50 @@
 'use strict';
 
+var fs = require( "fs" );
+var path = require( "path" );
+
+
 const Translate = {
 
     translate: function ( fromFolder, toFile ) {
         console.log( "fromFolder: " + fromFolder );
         console.log( "toFile: " + toFile );
+
+        this.readDirSync( fromFolder );
+
+        //usage:
+        this.readTextFile( "/Users/Documents/workspace/test.json", function ( text ) {
+            const data = JSON.parse( text );
+            console.log( data );
+        } )
     },
 
+
+    readDirSync: function ( path ) {
+        const pa = fs.readdirSync( path );
+        pa.forEach( function ( ele, index ) {
+            const info = fs.statSync( path + "/" + ele );
+            if ( info.isDirectory() ) {
+                console.log( "dir: " + ele );
+                this.readDirSync( path + "/" + ele );
+            } else {
+                console.log( "file: " + ele )
+            }
+        } )
+    },
+
+
+    readTextFile: function ( file, callback ) {
+        const rawFile = new XMLHttpRequest();
+        rawFile.overrideMimeType( "application/json" );
+        rawFile.open( "GET", file, true );
+        rawFile.onreadystatechange = function () {
+            if ( rawFile.readyState === 4 && rawFile.status == "200" ) {
+                callback( rawFile.responseText );
+            }
+        }
+        rawFile.send( null );
+    }
 };
 
 exports.Translate = Translate;
