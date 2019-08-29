bf408f9a9d679f946b39bd6c56b894e8de64c2a9
test <test@test.com>
2019-08-29 13:55:26 +0800

    update

--- main.js
+++ main.js
@@ -23,6 +23,7 @@ program.on( '--help', function () {
     console.log( '  $ translate-macos -f ~/codes/gtdollar-hh/data/locales -t ~/codes/gtdollar-hh/src/configs/Keys.js' );
     console.log( '  $ translate-macos -f ~/codes/exchange-webapp/src/Lang/locales -t ~/codes/exchange-webapp/src/Lang/Keys.js' );
     console.log( '  $ translate-macos -f ~/codes/gtins-app/data/locales -t ~/codes/gtins-app/src/configs/Keys.js' );
+    console.log( '  $ translate-macos -f ~/codes/gls-app/data/locales -t ~/codes/gls-app/src/configs/Keys.js' );
     console.log( '' );
     console.log( '' );
 } );
